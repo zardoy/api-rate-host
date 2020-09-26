92a2f3058a66954035a7f2cbb368f55a0d3c3e09
Vitaly Turovsky <vital2580@icloud.com>
2020-09-26 11:51:07 +0300

    style: remove commented lines

--- tests/nexus-test-environment.js
+++ tests/nexus-test-environment.js
@@ -4,9 +4,6 @@ const NodeEnvironment = require("jest-environment-node");
 
 class NexusEnvironment extends NodeEnvironment {
     connectionString = `postgresql://postgres:postgres@localhost:5432/ratehost`;
-    // postgresClient = new Client({
-    //     connectionString: this.connectionStringWithoutDb
-    // }); //connecting not to a db
     async setup() {
         this.global.process.env.TEST_USER_ID = "1806602625";
         this.global.process.env.DATABASE_URL = this.connectionString;
